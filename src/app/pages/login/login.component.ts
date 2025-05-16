15c0231592aaa241bf989d217a362a6158823734
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-05-16 11:03:58 -0300

    construindo açao de login e logout

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
-//import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 //import { LoginService } from '../../services/login.service';
 //import { ToastrService } from 'ngx-toastr';
@@ -12,19 +12,30 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
-    ReactiveFormsModule
-  ],
+    ReactiveFormsModule,
+    PrimaryInputComponent
+],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
-  construtor(){
+  
+  constructor(
+    private router: Router,
+    
+  ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
-      
     })
   }
+  submit() {
+    console.log(this.loginForm.value) 
+  }
+  navigate() {
+      this.router.navigate(["signup"]);
+  }
+  
   
 }
